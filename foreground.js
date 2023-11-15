1a199ca8bfc2a46f3cfb19196ee72b7755ceaa50
rstahl25 <135140579+rstahl25@users.noreply.github.com>
2023-11-15 00:24:04 +0000

    Add platforms and wall obstacle

--- foreground.js
+++ foreground.js
@@ -16,6 +16,29 @@ function game(){
   noStroke();
   fill(100, 200, 75);
   rect(width/2, 650, width, 200);
+
+  //Wall
+  stroke(0);
+  strokeWeight(10);
+  fill(0);
+  rect(1100, 370, 10, 350);
+
+  // Platforms
+  stroke(0);
+  strokeWeight(3);
+  fill(231, 149, 72);
+  rect(950, 300, 100, 10)
+
+  stroke(0);
+  strokeWeight(3);
+  fill(231, 149, 72);
+  rect(750, 400, 100, 10)
+
+  stroke(0);
+  strokeWeight(3);
+  fill(231, 149, 72);
+  rect(550, 500, 100, 10)
+
   // Window frame
   noFill();
   stroke(0);
