65b2daa9fc0937ddf1fa14a9d5b6e5610e314fdf
ryan-moorhead <149721627+ryan-moorhead@users.noreply.github.com>
2023-11-16 15:22:17 +0000

    added variable to help set the platform heights

--- foreground.js
+++ foreground.js
@@ -1,11 +1,8 @@
-
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
   textAlign(CENTER);
 }
-
 
 // Game function
 function game(){
@@ -15,26 +12,28 @@ function game(){
   fill(100, 200, 75);
   rect(width/2, height-(grassHeight/2), width, grassHeight);
 
+  var groundTop = height - grassHeight;
+  var jumpHeight = height/12;
   // Wall
   noStroke();
   fill(0)
-  rect(1100, height - (grassHeight * 1.62), 10, height/3)
+  rect(1100, groundTop - height/6, 10, height/3)
 
   // Platforms
   stroke(0);
   strokeWeight(3)
   fill(231, 149, 72)
-  rect(950, height - (grassHeight * 2), 130, 10)
+  rect(950, groundTop - (jumpHeight * 3) + 5, 130, 10)
 
   stroke(0);
   strokeWeight(3);
   fill(231, 149, 72);
-  rect(750, height - (grassHeight * 1.6), 130, 10)
+  rect(750, groundTop - (jumpHeight * 2) + 5, 130, 10)
 
   stroke(0);
   strokeWeight(3);
   fill(231, 149, 72);
-  rect(525, height - (grassHeight * 1.2), 130, 10)
+  rect(550, groundTop - (jumpHeight) + 5, 130, 10)
 
   // Window frame
   noFill();
