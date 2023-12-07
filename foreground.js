185c450a9ce967c1898e9a91a1e727738e325ce7
rstahl25 <135140579+rstahl25@users.noreply.github.com>
2023-12-07 21:37:17 +0000

    Put platform calls in level1.js file

--- foreground.js
+++ foreground.js
@@ -29,12 +29,6 @@ function game(){
   noStroke();
   fill(0)
   rect(1100, groundTop - height/6, 10, height/3)
-  
-  drawPlatform(950, (groundTop - (jumpHeight * 3) + 5), 130, 10, "#E79548");
-
-  drawPlatform(750, (groundTop - (jumpHeight * 2) + 5), 130, 10, "#E79548");
-
-  drawPlatform(550, (groundTop - (jumpHeight * 1) + 5), 130, 10, "#E79548");
 
   // Window frame
   noFill();
--- level1.js
+++ level1.js
@@ -1,29 +1,21 @@
-// Global variables for platforms
-height = 1100;
-grass_Height = 200;
-ground_Top = height - grass_Height;
-jump_Height = height/12;
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
   textAlign(CENTER);
-  
+
+  grass_Height = 200;
+  ground_Top = height - grass_Height;
+  jump_Height = height/12;
 }
+
 
 
 function draw() {
   // call function
     game();
-    drawPlatform(950, (ground_Top - (jump_Height * 3) + 5), 130, 10, "#E79548")
+    drawPlatform(950, (ground_Top - (jump_Height * 3) + 5), 130, 10, "#E79548");
+    drawPlatform(750, (ground_Top - (jump_Height * 2) + 5), 130, 10, "#E79548");
+    drawPlatform(550, (ground_Top - (jump_Height * 1) + 5), 130, 10, "#E79548");
     displayPlayer();
     keyPressed();
-}
-
-
-function drawPlatform(x, y, width, height, colorHex) {
-  stroke(0);
-  strokeWeight(3);
-  fill(colorHex)
-  rect(x, y, width, height);
 }
