e44f4e4bc707037713bba271d5fc763047e695fa
rstahl25 <135140579+rstahl25@users.noreply.github.com>
2023-12-09 15:51:42 +0000

    Move end goal code into its own file

--- foreground.js
+++ foreground.js
@@ -1,7 +1,3 @@
-//global variables for constructing end level building
-start_x = 1350;
-start_y = (groundTop - (grassHeight/2) - 150);
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
@@ -36,37 +32,11 @@ function game(){
   strokeWeight(15);
   rect(width/2, height/2, width, height);
 
-  //end building
-  noStroke();
-  fill(247, 194, 164);
-  rect(1425, (groundTop - (grassHeight/2) - 25), 150, 250);
+  // draw end building
+  var start_x = 1350;
+  var start_y = (ground_Top - (grass_Height/2) - 150);
+  drawEndGoal(groundTop, grassHeight, start_x, start_y);
 
-  noStroke();
-  noFill();
-  beginShape();
-  vertex(1350, (groundTop - (grassHeight/2) - 150));
-  vertex(1425, ((groundTop - (grassHeight/2)) - 150) - 100);
-  vertex(1500, (groundTop - (grassHeight/2)) - 150);
-  endShape();
-
-  noStroke();
-  fill(212, 219, 222);
-  for(let i = 1; i < 8; i++)
-  {
-    beginShape();
-    vertex(start_x + ((20 * i)/2), ((groundTop - (grassHeight/2) - 150)) - (((20 * i)/2) * 1.333));
-    vertex((start_x + (20 * i)), ((groundTop - (grassHeight/2) - 150)));
-    vertex((start_x + (20 * i) + 10), ((groundTop - (grassHeight/2) - 150)));
-    vertex(start_x + (((20 * i) + 10)/2), ((groundTop - (grassHeight/2) - 150)) - ((((20 * i) + 10)/2) * 1.333));
-    endShape(CLOSE);
-  }
-
-  brick(1415, (groundTop - (grassHeight/2) - 80), 30, 15);
-  brick(1390, (groundTop - (grassHeight/2) - 120), 30, 15);
-  brick(1480, (groundTop - (grassHeight/2) - 25), 30, 15);
-  brick(1485, (groundTop - (grassHeight/2) - 100), 30, 15);
-  brick(1375, (groundTop - (grassHeight/2) - 25), 30, 15);
-  brick(1430, (groundTop - (grassHeight/2) + 10), 30, 15);
 }
 
 
