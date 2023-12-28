4fb47766a3e3695982b112a5688d681eb47fc96f
rstahl25 <135140579+rstahl25@users.noreply.github.com>
2023-12-28 18:47:16 +0000

    Create a base for level 2 using p5play and sprites

---
+++ level2.js
@@ -0,0 +1,35 @@
+let platform, ball, grass;
+
+function setup() {
+    // Create canvas
+    canvas = createCanvas(windowWidth, windowHeight);
+    rectMode(CENTER);
+    textAlign(CENTER);
+
+    platform = new Sprite();
+    platform.width = 100;
+    platform.height = 20;
+    platform.collider = 's';
+    platform.rotation = 35;
+
+    ball = new Sprite();
+    ball.diameter = 100;
+    ball.x = windowWidth/2;
+    ball.y = windowHeight/3;
+    ball.collider = "d";
+
+    grass = new Sprite();
+    grass.width = windowWidth;
+    grass.height = windowHeight/2.5;
+    grass.x = windowWidth/2;
+    grass.y = windowHeight;
+    grass.collider = 's';
+    grass.color = 'green'
+
+
+    world.gravity.y = 10;
+}
+
+function draw() {
+    clear()
+}
