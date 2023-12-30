7751c177651cf12cdfa5d1c930fe35d033518170
rstahl25 <135140579+rstahl25@users.noreply.github.com>
2023-12-30 22:37:43 +0000

    Switch all sprite setup to use constructor

--- level2.js
+++ level2.js
@@ -1,4 +1,11 @@
-let platform, ball, grass;
+// Set up sprite variables for level
+let platform1, platform2, platform3, wall, char, grass;
+
+// Set up variables for player rendering
+var p1X = 130;
+var p1Y = 525;
+
+
 
 function setup() {
     // Create canvas
@@ -6,24 +13,28 @@ function setup() {
     rectMode(CENTER);
     textAlign(CENTER);
 
-    platform = new Sprite();
-    platform.width = 100;
-    platform.height = 20;
-    platform.collider = 's';
-    platform.rotation = 15;
+    // Global foreground variables
+    grass_Height = 200;
+    ground_Top = height - grass_Height;
+    jump_Height = height/12;
+    start_x = 1350;
+    start_y = (ground_Top - (grass_Height/2) - 150);
 
-    ball = new Sprite();
-    ball.diameter = 100;
-    ball.x = windowWidth/2;
-    ball.y = windowHeight/3;
-    ball.collider = "d";
+    platform1 = new Sprite(950, (ground_Top - (jump_Height * 3) + 5), 140, 10, 's');
+    platform1.color = '#E79548';
 
-    grass = new Sprite();
-    grass.width = windowWidth;
-    grass.height = windowHeight/2.5;
-    grass.x = windowWidth/2;
-    grass.y = windowHeight;
-    grass.collider = 's';
+    platform2 = new Sprite(700, (ground_Top - (jump_Height * 2) + 5), 140, 10, 's');
+    platform2.color = '#E79548';
+
+    platform3 = new Sprite(450, (ground_Top - (jump_Height * 1) + 5), 140, 10, 's');
+    platform3.color = '#E79548';
+
+    wall = new Sprite(1100, (ground_Top - height/6), 10, height/2, 's');
+    wall.color = 'black';
+
+    char = new Sprite(p1X, p1Y, 60, 120, 'd');
+
+    grass = new Sprite(windowWidth/2, windowHeight, windowWidth, windowHeight/2.5, 's');
     grass.color = 'green'
 
 
