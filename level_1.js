ce22c8dd71f58c90b42b859dcfa7daf84517ec8d
rstahl25 <135140579+rstahl25@users.noreply.github.com>
2024-01-12 18:21:03 +0000

    Add lava obstacle

--- level_1.js
+++ level_1.js
@@ -1,5 +1,5 @@
 // Set up sprite variables for level
-let platform3, wall, player, grass, grass2;
+let platform3, wall, player, grass, grass2, lava;
 
 // Set up variables for player starting position
 var p1X = 130;
@@ -19,6 +19,7 @@ function setup() {
     jump_Height = height/12;
     start_x = 1350;
     start_y = (ground_Top - (grass_Height/2) - 150);
+    let windowWidthRemaining = windowWidth;
     
 
     // Create platforms
@@ -34,8 +35,19 @@ function setup() {
     player.color = 'blue'
 
     // Create grass platform
-    grass = new Sprite(0, windowHeight, windowWidth/2, windowHeight/2.5, 's');
+    // the positioning of the separated platform must be a starting x value 
+    //    of half the platform's width since the (x,y) is based on the center of the sprite
+    grass = new Sprite(windowWidth/8, windowHeight, windowWidth/4, windowHeight/2.5, 's');
     grass.color = 'green'
+    grass.friction = 0;
+    let grassEnd = (windowWidth - (windowWidth/8 + windowWidth/4))
+    windowWidthRemaining -= (windowWidth/4);
+
+    grass2 = new Sprite(windowWidth, windowHeight, windowWidthRemaining, windowHeight/2.5, 's')
+    grass2.color = 'grey'
+
+    lava = new Sprite(grassEnd, windowHeight, windowWidthRemaining, windowHeight/32, 's')
+    lava.color = 'red'
 
 
     // Establish world gravity
