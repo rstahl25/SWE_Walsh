1ee22c413dd5cf083c83ae18571f5770528ad609
rstahl25 <135140579+rstahl25@users.noreply.github.com>
2024-01-14 19:44:24 +0000

    Removed hard-coded values in tutorial level

--- tutorial.js
+++ tutorial.js
@@ -1,10 +1,6 @@
 // Set up sprite variables for level
 let platform1, platform2, platform3, wall, player, grass;
 
-// Set up variables for player starting position
-var p1X = 130;
-var p1Y = 525;
-
 
 
 function setup() {
@@ -19,22 +15,24 @@ function setup() {
     jump_Height = height/12;
     start_x = 1350;
     start_y = (ground_Top - (grass_Height/2) - 150);
+    p1X = windowWidth/20;
+    p1Y = windowHeight - windowHeight/3;
 
     // Create platforms
-    platform1 = new Sprite(windowWidth/1.75, (ground_Top - (jump_Height * 3) + 5), 140, 10, 's');
+    platform1 = new Sprite(windowWidth/1.75, (windowHeight - windowHeight/1.75), 140, 10, 's');
     platform1.color = '#E79548';
     platform1.friction = 0;
 
-    platform2 = new Sprite(windowWidth/2.25, (ground_Top - (jump_Height * 2) + 5), 140, 10, 's');
+    platform2 = new Sprite(windowWidth/2.25, (windowHeight - windowHeight/2.25), 140, 10, 's');
     platform2.color = '#E79548';
     platform2.friction = 0;
 
-    platform3 = new Sprite(windowWidth/3.25, (ground_Top - (jump_Height * 1) + 5), 140, 10, 's');
+    platform3 = new Sprite(windowWidth/3.25, (windowHeight - windowHeight/3), 140, 10, 's');
     platform3.color = '#E79548';
     platform3.friction = 0;
 
     // Create wall obstacle
-    wall = new Sprite(windowWidth/1.4, (ground_Top - height/6), 10, height/2, 's');
+    wall = new Sprite(windowWidth/1.4, (windowHeight - windowHeight/2.25), 10, windowHeight/2, 's');
     wall.color = 'black';
     wall.friction = 0;
 
@@ -76,22 +74,22 @@ function draw() {
     // Allow player vertical movement with jump limitation
     if (kb.presses('up') && (player.colliding(grass))) {
       player.bearing = -90;
-      player.applyForce(700);
+      player.applyForce(650);
     }
 
     if (kb.presses('up') && (player.colliding(platform3))) {
         player.bearing = -90;
-        player.applyForce(700);
+        player.applyForce(600);
     }
 
     if (kb.presses('up') && (player.colliding(platform2))) {
         player.bearing = -90;
-        player.applyForce(700);
+        player.applyForce(600);
     }
 
     if (kb.presses('up') && (player.colliding(platform1))) {
         player.bearing = -90;
-        player.applyForce(700);
+        player.applyForce(600);
     }
 
     clear();
