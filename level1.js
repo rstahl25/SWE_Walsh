8f52879d42467c17d369b7f516c9c334fdc0f782
rstahl25 <135140579+rstahl25@users.noreply.github.com>
2024-01-17 23:09:13 +0000

    Fixed movement with sprites by getting them from the array of objects

--- level1.js
+++ level1.js
@@ -71,70 +71,10 @@ function setupScene(windowWidth, windowHeight, objects) {
 function setup() {
     scene = createObjects(windowWidth, windowHeight)
     setupScene(windowWidth, windowHeight, scene);
+
+    p1X = windowWidth/20;
+    p1Y = windowHeight - windowHeight/3;
 }
-
-// function setup() {
-//     // Create canvas
-//     createCanvas(windowWidth, windowHeight);
-//     rectMode(CENTER);
-//     textAlign(CENTER);
-
-//      // Global foreground variables
-
-//      p1X = windowWidth/20;
-//      p1Y = windowHeight - windowHeight/3;
-//      let windowWidthRemaining = windowWidth;
-
-
-//      // Create player sprite
-//     player = new Sprite(p1X, p1Y, 30, 40, 'd');
-//     player.rotationLock = true;
-//     //player.img = 'img/walsh.JPG'
-//     player.color = 'blue'
-
-//      // Create grass platform
-//     // the positioning of the separated platform must be a starting x value 
-//     //    of half the platform's width since the (x,y) is based on the center of the sprite
-//     grass = new Sprite(windowWidth/8, windowHeight, windowWidth/4, windowHeight/2.5, 's');
-//     grass.color = 'green'
-//     grass.friction = 0;
-//     let grassEnd = ((windowWidth/8 + windowWidth/4))
-//     windowWidthRemaining -= (windowWidth/4);
-
-//      // Create lava obstacle
-//     lava = new Sprite(grassEnd, windowHeight, windowWidth/4, windowHeight/32, 's')
-//     lava.color = 'red'
-//     windowWidthRemaining -= (windowWidth/4)
-//     let lavaEnd = grassEnd + (windowWidth/8 + windowWidth/4)
-
-//      //Create second grass platform
-//     grass2 = new Sprite(lavaEnd, windowHeight, windowWidthRemaining, windowHeight/2.5, 's')
-//     grass2.color = 'green'
-//     grass2.friction = 0;
-
-//      // Create platforms
-//     platform = new Sprite(grassEnd, (windowHeight - windowHeight/5), windowWidth/25, 10, 's');
-//     platform.color = '#E79548';
-//     platform.friction = 0;
-
-//      platform1 = new Sprite(windowWidth - windowWidth/2.5, (windowHeight - windowHeight/3.5), windowWidth/25, 10, 's');
-//     platform1.color = '#E79548';
-//     platform1.friction = 0;
-
-//     platform2 = new Sprite(windowWidth - windowWidth/3.5, (windowHeight - windowHeight/2.75), windowWidth/25, 10, 's');
-//     platform2.color = '#E79548';
-//     platform2.friction = 0;
-
-//      //Create goal platform
-//     goal = new Sprite(windowWidth - windowWidth/10, (windowHeight - windowHeight/2.25), windowWidth/5, 10, 's');
-//     goal.color = 'black';
-//     goal.friction = 0;
-
-
-//      // Establish world gravity
-//      world.gravity.y = 10;
-
-//  }
 
 
 function draw() {
@@ -160,71 +100,71 @@ function draw() {
         objects[objectNames.indexOf('player')].vel.x = 0;
     }
        
-    // // Allow player vertical movement with jump limitation
-    // if (kb.presses('up') && (player.colliding(grass))) {
-    //   player.bearing = -90;
-    //   player.applyForce(550);
-    // }
+    // Allow player vertical movement with jump limitation
+    if (kb.presses('up') && (objects[objectNames.indexOf('player')].colliding(objects[objectNames.indexOf('grass')]))) {
+        objects[objectNames.indexOf('player')].bearing = -90;
+        objects[objectNames.indexOf('player')].applyForce(550);
+    }
 
-    // if (kb.presses('up') && (player.colliding(platform))) {
-    //     player.bearing = -90;
-    //     player.applyForce(550);
-    // }
+    if (kb.presses('up') && (objects[objectNames.indexOf('player')].colliding(objects[objectNames.indexOf('platform')]))) {
+        objects[objectNames.indexOf('player')].bearing = -90;
+        objects[objectNames.indexOf('player')].applyForce(550);
+    }
 
-    // if (kb.presses('up') && (player.colliding(platform1))) {
-    //     player.bearing = -90;
-    //     player.applyForce(550);
-    // }
+    if (kb.presses('up') && (objects[objectNames.indexOf('player')].colliding(objects[objectNames.indexOf('platform1')]))) {
+        objects[objectNames.indexOf('player')].bearing = -90;
+        objects[objectNames.indexOf('player')].applyForce(550);
+    }
 
-    // if (kb.presses('up') && (player.colliding(platform2))) {
-    //     player.bearing = -90;
-    //     player.applyForce(550);
-    // }
+    if (kb.presses('up') && (objects[objectNames.indexOf('player')].colliding(objects[objectNames.indexOf('platform2')]))) {
+        objects[objectNames.indexOf('player')].bearing = -90;
+        objects[objectNames.indexOf('player')].applyForce(550);
+    }
 
-    // if (kb.presses('up') && (player.colliding(grass2))) {
-    //     player.bearing = -90;
-    //     player.applyForce(550);
-    // }
+    if (kb.presses('up') && (objects[objectNames.indexOf('player')].colliding(objects[objectNames.indexOf('grass2')]))) {
+        objects[objectNames.indexOf('player')].bearing = -90;
+        objects[objectNames.indexOf('player')].applyForce(550);
+    }
 
-    // if (kb.presses('up') && (player.colliding(goal))) {
-    //     player.bearing = -90;
-    //     player.applyForce(550);
-    // }
+    if (kb.presses('up') && (objects[objectNames.indexOf('player')].colliding(objects[objectNames.indexOf('goal')]))) {
+        objects[objectNames.indexOf('player')].bearing = -90;
+        objects[objectNames.indexOf('player')].applyForce(550);
+    }
 
-    // if(player.collides(lava)) {
-    //     player.x = p1X;
-    //     player.y = p1Y;
-    // }
+    if(objects[objectNames.indexOf('player')].collides(objects[objectNames.indexOf('lava')])) {
+        objects[objectNames.indexOf('player')].x = p1X;
+        objects[objectNames.indexOf('player')].y = p1Y;
+    }
 
-    // // // Allow user to pause and resume game using SPACE.
-    // if (kb.presses('Space')) {
-    //     if (pause === true) {
-    //         pause = false;
-    //     }
+    // Allow user to pause and resume game using SPACE.
+    if (kb.presses('Space')) {
+        if (pause === true) {
+            pause = false;
+        }
 
-    //     else {
-    //         pause = true;
-    //     }
-    // }
+        else {
+            pause = true;
+        }
+    }
 
-    // if (pause === true) {
-    //     player.sleeping = true;
-    //     removeElements();
-    //     let p1 = createElement('h2', 'Game Paused');
-    //     p1.position(windowWidth/2, (windowHeight/2) - 20);
-    //     p1.attribute('align', 'center');
-    //     let p2 = createElement('h2', 'Press SPACE to Resume');
-    //     p2.position(windowWidth/2, (windowHeight/2) + 20);
-    //     p2.attribute('align', 'center');
-    // }
+    if (pause === true) {
+        objects[objectNames.indexOf('player')].sleeping = true;
+        removeElements();
+        let p1 = createElement('h2', 'Game Paused');
+        p1.position(windowWidth/2, (windowHeight/2) - 20);
+        p1.attribute('align', 'center');
+        let p2 = createElement('h2', 'Press SPACE to Resume');
+        p2.position(windowWidth/2, (windowHeight/2) + 20);
+        p2.attribute('align', 'center');
+    }
 
-    // else {
-    //     player.sleeping = false;
-    //     removeElements();
-    //     let r = createElement('h2', 'Press SPACE to Pause');
-    //     r.position(50, 10);
-    //     r.attribute('align', 'center');
-    // }
+    else {
+        objects[objectNames.indexOf('player')].sleeping = false;
+        removeElements();
+        let r = createElement('h2', 'Press SPACE to Pause');
+        r.position(50, 10);
+        r.attribute('align', 'center');
+    }
 
 
     clear();
