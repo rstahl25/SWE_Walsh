649608c32dcddfd90bcf45895617ab435b2d323a
rstahl25 <135140579+rstahl25@users.noreply.github.com>
2024-01-17 01:15:43 +0000

    Fix typos in comments

--- level1.js
+++ level1.js
@@ -28,7 +28,7 @@ function createObjects(windowWidth, windowHeight) {
     objects.push(createObject('platform2', (windowWidth - windowWidth/3.5), (windowHeight - windowHeight/2.75), windowWidth/25, 10, '#E79548', 0, null, 's', 0, 0))
     objects.push(createObject('platform1', (windowWidth - windowWidth/2.5), (windowHeight - windowHeight/3.5), windowWidth/25, 10, '#E79548', 0, null, 's', 0, 0))
     objects.push(createObject('goal', (windowWidth - windowWidth/10), (windowHeight - windowHeight/2.25), windowWidth/5, 10, 'black', 0, null, 's', 0, 0))
-    //objects.push(createObject('player', windowWidth/20, (windowHeight - windowHeight/3), 30, 40, 'blue', 0, null, 'd', 0, 0))
+    objects.push(createObject('player', windowWidth/20, (windowHeight - windowHeight/3), 30, 40, 'blue', 0, null, 'd', 0, 0))
     return objects;
 }
 
@@ -41,10 +41,10 @@ function setupScene(windowWidth, windowHeight, objects) {
     rectMode(CENTER);
     textAlign(CENTER);
 
-    player = new Sprite(windowWidth/20, (windowHeight - windowHeight/3), 30, 40, 'd');
-    player.rotationLock = true;
-    //player.img = 'img/walsh.JPG'
-    player.color = 'blue'
+    // player = new Sprite(windowWidth/20, (windowHeight - windowHeight/3), 30, 40, 'd');
+    // player.rotationLock = true;
+    // player.img = 'img/walsh.JPG'
+    // player.color = 'blue'
 
 
 
@@ -65,30 +65,94 @@ function setupScene(windowWidth, windowHeight, objects) {
 function setup() {
     scene = createObjects(windowWidth, windowHeight)
     setupScene(windowWidth, windowHeight, scene);
-
-    draw()
-    
 }
+
+//function setup() {
+    // Create canvas
+//     createCanvas(windowWidth, windowHeight);
+//     rectMode(CENTER);
+//     textAlign(CENTER);
+
+//     // Global foreground variables
+//     grass_Height = 200;
+//     ground_Top = height - grass_Height;
+//     jump_Height = height/12;
+//     start_x = 1350;
+//     start_y = (ground_Top - (grass_Height/2) - 150);
+//     p1X = windowWidth/20;
+//     p1Y = windowHeight - windowHeight/3;
+//     let windowWidthRemaining = windowWidth;
+
+
+//     // Create player sprite
+//     player = new Sprite(p1X, p1Y, 30, 40, 'd');
+//     player.rotationLock = true;
+//     //player.img = 'img/walsh.JPG'
+//     player.color = 'blue'
+
+//     // Create grass platform
+//     // the positioning of the separated platform must be a starting x value 
+//     //    of half the platform's width since the (x,y) is based on the center of the sprite
+//     grass = new Sprite(windowWidth/8, windowHeight, windowWidth/4, windowHeight/2.5, 's');
+//     grass.color = 'green'
+//     grass.friction = 0;
+//     let grassEnd = ((windowWidth/8 + windowWidth/4))
+//     windowWidthRemaining -= (windowWidth/4);
+
+//     // Create lava obstacle
+//     lava = new Sprite(grassEnd, windowHeight, windowWidth/4, windowHeight/32, 's')
+//     lava.color = 'red'
+//     windowWidthRemaining -= (windowWidth/4)
+//     let lavaEnd = grassEnd + (windowWidth/8 + windowWidth/4)
+
+//     //Create second grass platform
+//     grass2 = new Sprite(lavaEnd, windowHeight, windowWidthRemaining, windowHeight/2.5, 's')
+//     grass2.color = 'green'
+//     grass2.friction = 0;
+
+//     // Create platforms
+//     platform = new Sprite(grassEnd, (windowHeight - windowHeight/5), windowWidth/25, 10, 's');
+//     platform.color = '#E79548';
+//     platform.friction = 0;
+
+//     platform1 = new Sprite(windowWidth - windowWidth/2.5, (windowHeight - windowHeight/3.5), windowWidth/25, 10, 's');
+//     platform1.color = '#E79548';
+//     platform1.friction = 0;
+
+//     platform2 = new Sprite(windowWidth - windowWidth/3.5, (windowHeight - windowHeight/2.75), windowWidth/25, 10, 's');
+//     platform2.color = '#E79548';
+//     platform2.friction = 0;
+
+//     //Create goal platform
+//     goal = new Sprite(windowWidth - windowWidth/10, (windowHeight - windowHeight/2.25), windowWidth/5, 10, 's');
+//     goal.color = 'black';
+//     goal.friction = 0;
+
+
+//     // Establish world gravity
+//     world.gravity.y = 10;
+
+// }
 
 
 function draw() {
 
     // Allow player horizontal movement
-    if(kb.pressing('left')) {
-        if(player.x < 10) {
-            player.vel.x = 5;
-        } else {
-            player.vel.x = -5;
-        }
-    } else if (kb.pressing('right')) {
-        if(player.x > windowWidth - 10) {
-            player.vel.x = -5;
-        } else {
-            player.vel.x = 5;
-        }
-    } else {
-        player.vel.x = 0;
-    }
+    // if(kb.pressing('left')) {
+    //     if(player.x < 10) {
+    //         player.vel.x = 5;
+    //     } else {
+    //         player.vel.x = -5;
+    //     }
+    // } else if (kb.pressing('right')) {
+    //     if(player.x > windowWidth - 10) {
+    //         player.vel.x = -5;
+    //     } else {
+    //         player.vel.x = 5;
+    //     }
+    // } else {
+    //     player.vel.x = 0;
+    // }
        
     // Allow player vertical movement with jump limitation
     // if (kb.presses('up') && (player.colliding(grass))) {
